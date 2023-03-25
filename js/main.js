0aa8e0d84fd480a05d5da5b03ebe5b8c16b707f2
Quentin <quentin01.bource@gmail.com>
2023-03-25 15:20:18 +0100

    camps rouge ok

--- js/main.js
+++ js/main.js
@@ -25,6 +25,60 @@ var camera;
 
 function preload(){
 
+    //intégralité des choses qu'on retrouve dans un camps
+    this.load.image("tentebleue", "assets/decor/tente_bleue.png")
+    this.load.image("tentejaune", "assets/decor/tente_jaune.png")
+    this.load.image("tentemauve", "assets/decor/tente_mauve.png")
+    this.load.image("tenterouge", "assets/decor/tente_rouge.png")
+    this.load.image("pilotibleue", "assets/decor/tentePilotisBleue.png")
+    this.load.image("pilotijaune", "assets/decor/tentePilotisJaune.png")
+    this.load.image("pilotimauve", "assets/decor/tentePilotisMauve.png")
+    this.load.image("pilotirouge", "assets/decor/tentePilotisRouge.png")
+    this.load.image("chiotte", "assets/decor/Chiottes.png")
+    this.load.image("rondin", "assets/decor/Rondins.png")
+    this.load.image("tableafeu" , "assets/decor/tbaf.png")
+    this.load.image("chiotteR" , "assets/decor/chiotteR.png")
+    this.load.image("chiotteB" , "assets/decor/chiotteB.png")
+    this.load.image("chiotteJ" , "assets/decor/chiotteJ.png")
+    this.load.image("chiotteM" , "assets/decor/chiotteM.png")
+    this.load.image("bassin" , "assets/decor/bassin.png")
+    this.load.image("bassinGold" , "assets/decor/bassinGold.png")
+    this.load.image("gourde" , "assets/decor/gourde.png")
+    this.load.image("gourdeGold" , "assets/decor/gourdeGold.png")
+
+
+
+
+
+
+
+
+
+
+    //animation feu
+    this.load.image("fire1", "assets/decor/fire1.png")
+    this.load.image("fire2", "assets/decor/fire2.png")
+    this.load.image("fire3", "assets/decor/fire3.png")
+    this.load.image("fire4", "assets/decor/fire4.png")
+
+    //animation vache
+    this.load.image("cow1", "assets/animals/cow1.png")
+    this.load.image("cow2", "assets/animals/cow2.png")
+    this.load.image("cow3", "assets/animals/cow3.png")
+    this.load.image("cow4", "assets/animals/cow4.png")
+
+    //animation chicken
+    this.load.image("chicken1", "assets/animals/chicken1.png")
+    this.load.image("chicken2", "assets/animals/chicken2.png")
+    this.load.image("chicken3", "assets/animals/chicken3.png")
+    this.load.image("chicken4", "assets/animals/chicken4.png")
+
+    //animation pig 
+    this.load.image("pig1", "assets/animals/pig1.png")
+    this.load.image("pig2", "assets/animals/pig2.png")
+    this.load.image("pig3", "assets/animals/pig3.png")
+    this.load.image("pig4", "assets/animals/pig4.png")
+
     //background
     this.load.image("background", "assets/perso/background.png")
 
@@ -86,6 +140,96 @@ function create(){
     this.cameras.main.setBounds(0, 0, 1200, 1200);
     this.cameras.main.setZoom(2);
     this.physics.world.setBounds(0, 0, 1000, 1000);
+
+    //création du camps rouge complet 
+    this.add.sprite(100, 50, "tenterouge")
+    this.add.sprite(120, 90, "pilotirouge")
+    chiotte1 = this.add.sprite(300, 60, "chiotte")
+    chiotte1.setScale(0.75)
+    rondin1 = this.add.sprite(300, 170, "rondin")
+     tableafeu1 = this.add.sprite(300, 180, "tableafeu")
+    chiotteR = this.add.sprite(300, 55, "chiotteR")
+    chiotteR.setScale(0.75)
+    gourde1 = this.add.sprite(425, 180, "gourde")
+    gourde1.setScale(0.75)
+    gourdeGold1 = this.add.sprite(425, 180, "gourdeGold")
+    gourdeGold1.setScale(0.75)
+    bassin1 = this.add.sprite(150, 200, "bassin")
+    bassin1.setScale(0.75)
+    bassinGold1 = this.add.sprite(150, 200, "bassinGold")
+    bassinGold1.setScale(0.75)
+
+    //création du camps bleue complet 
+    tentebleue = this.add.sprite(870, 50, "tentebleue")
+    tentebleue.setScale(-1,1)
+    pilotibleue = this.add.sprite(860, 90, "pilotibleue")
+    pilotibleue.setScale(-1,1)
+    chiotte2 = this.add.sprite(300, 60, "chiotte")
+    chiotte2.setScale(0.75)
+    rondin1 = this.add.sprite(300, 170, "rondin")
+    tableafeu1 = this.add.sprite(300, 180, "tableafeu")
+    chiotteR = this.add.sprite(300, 55, "chiotteR")
+    chiotteR.setScale(0.75)
+    gourde1 = this.add.sprite(425, 180, "gourde")
+    gourde1.setScale(0.75)
+    gourdeGold1 = this.add.sprite(425, 180, "gourdeGold")
+    gourdeGold1.setScale(0.75)
+    bassin1 = this.add.sprite(150, 200, "bassin")
+    bassin1.setScale(0.75)
+    bassinGold1 = this.add.sprite(150, 200, "bassinGold")
+    bassinGold1.setScale(0.75)
+
+
+     //création feu
+     this.anims.create({
+        key : "fire", 
+        frames : [
+            {key : "fire1"},
+            {key : "fire2"},
+            {key : "fire3"},
+            {key : "fire4"},
+        ],
+        frameRate : 4,
+        repeat : -1
+    })
+
+    //création animaux
+    this.anims.create({
+        key : "cow", 
+        frames : [
+            {key : "cow1"},
+            {key : "cow2"},
+            {key : "cow3"},
+            {key : "cow4"},
+        ],
+        frameRate : 6,
+        repeat : -1
+    })
+
+    this.anims.create({
+        key : "pig", 
+        frames : [
+            {key : "pig1"},
+            {key : "pig2"},
+            {key : "pig3"},
+            {key : "pig4"},
+        ],
+        frameRate : 6,
+        repeat : -1
+    })
+
+    this.anims.create({
+        key : "chicken", 
+        frames : [
+            {key : "chicken1"},
+            {key : "chicken2"},
+            {key : "chicken3"},
+            {key : "chicken4"},
+        ],
+        frameRate : 6,
+        repeat : -1
+    })
+
 
     //création des animation pour le joueur
     this.anims.create({
@@ -182,6 +326,22 @@ function create(){
     tree5.anims.play("tree")
     tree6 = this.physics.add.sprite( 800 , 600, "tree")
     tree6.anims.play("tree")
+
+    //animaux 
+    cow1 = this.physics.add.sprite(120, 290, "cow")
+    cow1.anims.play("cow")
+    cow1.setScale(3)
+    pig1 = this.physics.add.sprite(190, 350, "pig")
+    pig1.anims.play("pig")
+    pig1.setScale(3)
+    chicken1 = this.physics.add.sprite(270, 250, "chicken")
+    chicken1.anims.play("chicken")
+    chicken1.setScale(3)
+
+    //fire
+    fire1 = this.physics.add.sprite(300, 150, "fire")
+    fire1.anims.play("fire")
+    fire1.setScale(0.1)
 
     //appartion du joueur
     joueur = this.physics.add.sprite( 500 , 550, "joueur")
